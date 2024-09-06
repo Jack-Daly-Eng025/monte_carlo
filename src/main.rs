a7e64d878dc760ca440991e0b206654df8b59ff2
Jack-Daly-Eng025 <jac.daly.2021@outlook.com>
2024-09-06 16:08:21 +0100

    commit changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+//A monte-carlo simulation. 
 use rand::Rng;
 
 
@@ -5,10 +6,10 @@ fn esti_pi(n_samp: usize) -> f64{
     let mut rng = rand::thread_rng();
     let mut inner_circ = 0;
 
-    for i in 0..n_samp{
-        let x = rng.gen();
-        let y = rng.gen();
-        if x * x + y* y < = 1.0 {
+    for _ in 0..n_samp{
+        let x : f64 = rng.gen();
+        let y : f64 = rng.gen();
+        if x * x + y* y <= 1.0 {
             inner_circ +=1;
         }
     }
@@ -17,7 +18,10 @@ fn esti_pi(n_samp: usize) -> f64{
 }
 
 fn main() {
-    println!("Hello, world!");
+    let n = 10000;
+
+    let  pi_esti = esti_pi(n);
+    println!("The estimated value of pi is : {}", pi_esti);
 }
 
 
